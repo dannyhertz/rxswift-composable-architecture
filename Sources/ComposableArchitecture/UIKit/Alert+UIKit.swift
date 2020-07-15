765a4032d900d625e8eacdfbf09a7965fac9cc41
dannyhertz <dannyhertz@users.noreply.github.com>
2020-07-15 18:50:54 +0000

    Run swift-format

--- Sources/ComposableArchitecture/UIKit/Alert+UIKit.swift
+++ Sources/ComposableArchitecture/UIKit/Alert+UIKit.swift
@@ -6,43 +6,45 @@
 //
 
 #if canImport(UIKit)
-import Foundation
-import UIKit
+  import Foundation
+  import UIKit
 
-extension AlertState.Button {
+  extension AlertState.Button {
     func toUIKit(send: @escaping (Action) -> Void) -> UIAlertAction {
-        let action = { if let action = self.action { send(action) } }
-        switch self.type {
-        case let .cancel(.some(label)):
-            return .init(title: label, style: .cancel, handler: { _ in action() })
-        case .cancel(.none):
-            return .init(title: nil, style: .cancel, handler: { _ in action() })
-        case let .default(label):
-            return .init(title: label, style: .default, handler: { _ in action() })
-        case let .destructive(label):
-            return .init(title: label, style: .destructive, handler: { _ in action() })
-        }
+      let action = { if let action = self.action { send(action) } }
+      switch self.type {
+      case let .cancel(.some(label)):
+        return .init(title: label, style: .cancel, handler: { _ in action() })
+      case .cancel(.none):
+        return .init(title: nil, style: .cancel, handler: { _ in action() })
+      case let .default(label):
+        return .init(title: label, style: .default, handler: { _ in action() })
+      case let .destructive(label):
+        return .init(title: label, style: .destructive, handler: { _ in action() })
+      }
     }
-}
+  }
 
-extension AlertState {
+  extension AlertState {
     func toUIKit(send: @escaping (Action) -> Void) -> UIAlertController {
-        let alert =  UIAlertController(title: title, message: message, preferredStyle: .alert)
+      let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
 
-        if let primaryButton = primaryButton {
-            alert.addAction(primaryButton.toUIKit(send: send))
-        }
-        if let secondaryButton = secondaryButton {
-            alert.addAction(secondaryButton.toUIKit(send: send))
-        }
+      if let primaryButton = primaryButton {
+        alert.addAction(primaryButton.toUIKit(send: send))
+      }
+      if let secondaryButton = secondaryButton {
+        alert.addAction(secondaryButton.toUIKit(send: send))
+      }
 
-        return alert
+      return alert
     }
-}
+  }
 
-public extension UIAlertController {
-    static func alert<Action>(_ alert: AlertState<Action>, send: @escaping (Action) -> Void) -> UIAlertController {
-        return alert.toUIKit(send: send)
+  extension UIAlertController {
+    public static func alert<Action>(_ alert: AlertState<Action>, send: @escaping (Action) -> Void)
+      -> UIAlertController
+    {
+      return alert.toUIKit(send: send)
     }
-}
+  }
 #endif
